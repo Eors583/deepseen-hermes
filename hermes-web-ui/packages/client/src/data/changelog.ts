447f2b37c01f696a64ca5f7ee00471aa93dd71ad
Eros583 <1193254370@qq.com>
2026-06-15 15:48:51 +0800

    gx

---
+++ hermes-web-ui/packages/client/src/data/changelog.ts
@@ -0,0 +1,72 @@
+export interface ChangelogEntry {
+  version: string
+  date: string
+  changes: string[]
+}
+
+export const changelog: ChangelogEntry[] = [
+  {
+    version: '0.6.14',
+    date: '2026-06-11',
+    changes: [
+      'changelog.new_0_6_14_1',
+      'changelog.new_0_6_14_2',
+      'changelog.new_0_6_14_3',
+      'changelog.new_0_6_14_4',
+      'changelog.new_0_6_14_5',
+      'changelog.new_0_6_14_6',
+      'changelog.new_0_6_14_7',
+      'changelog.new_0_6_14_8',
+      'changelog.new_0_6_14_9',
+      'changelog.new_0_6_14_10',
+    ],
+  },
+  {
+    version: '0.6.12',
+    date: '2026-06-09',
+    changes: [
+      'changelog.new_0_6_12_1',
+      'changelog.new_0_6_12_2',
+      'changelog.new_0_6_12_3',
+      'changelog.new_0_6_12_4',
+      'changelog.new_0_6_12_5',
+      'changelog.new_0_6_12_6',
+      'changelog.new_0_6_12_7',
+      'changelog.new_0_6_12_8',
+      'changelog.new_0_6_12_9',
+      'changelog.new_0_6_12_10',
+    ],
+  },
+  {
+    version: '0.6.11',
+    date: '2026-06-06',
+    changes: [
+      'changelog.new_0_6_11_1',
+      'changelog.new_0_6_11_2',
+      'changelog.new_0_6_11_3',
+      'changelog.new_0_6_11_4',
+      'changelog.new_0_6_11_5',
+      'changelog.new_0_6_11_6',
+      'changelog.new_0_6_11_7',
+      'changelog.new_0_6_11_8',
+      'changelog.new_0_6_11_9',
+      'changelog.new_0_6_11_10',
+    ],
+  },
+  {
+    version: '0.6.10',
+    date: '2026-06-03',
+    changes: [
+      'changelog.new_0_6_10_1',
+      'changelog.new_0_6_10_2',
+      'changelog.new_0_6_10_3',
+      'changelog.new_0_6_10_4',
+      'changelog.new_0_6_10_5',
+      'changelog.new_0_6_10_6',
+      'changelog.new_0_6_10_7',
+      'changelog.new_0_6_10_8',
+      'changelog.new_0_6_10_9',
+      'changelog.new_0_6_10_10',
+    ],
+  },
+]
